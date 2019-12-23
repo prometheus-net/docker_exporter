3c028704f25040b0aa3ae54df008e2113b189eb6
Sander Saares <sander@saares.eu>
2019-12-23 19:20:33 +0200

    Branch management and version strings

--- .github/actions/make-version-string/entrypoint.ps1
+++ .github/actions/make-version-string/entrypoint.ps1
@@ -4,5 +4,5 @@ Import-Module Axinom.DevOpsTooling
 
 $path = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_ASSEMBLYINFOPATH
 
-$version = Set-DotNetBuildAndVersionStrings -assemblyInfoPath $path -commitId $ENV:GITHUB_SHA
+$version = Set-DotNetBuildAndVersionStrings -assemblyInfoPath $path -commitId $ENV:GITHUB_SHA -primaryBranchName $env:INPUT_PRIMARYBRANCH
 Write-Host "::set-output name=versionstring::$version"
