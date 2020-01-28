e432961cdf7bf9a25bc34b45de29a0e72aa2b763
Sander Saares <sander@saares.eu>
2020-01-28 15:28:27 +0200

    Replace actions with externally hosted

--- .github/actions/expand-tokens/entrypoint.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-Import-Module Axinom.DevOpsTooling
-
-$path = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_PATH
-
-Expand-Tokens -path $path -filenames $env:INPUT_FILENAMES -recursive:([bool]$env:INPUT_RECURSIVE)
--- .github/actions/make-version-string/entrypoint.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-Import-Module Axinom.DevOpsTooling
-
-$path = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_ASSEMBLYINFOPATH
-
-$version = Set-DotNetBuildAndVersionStrings -assemblyInfoPath $path -commitId $ENV:GITHUB_SHA -primaryBranchName $env:INPUT_PRIMARYBRANCH
-Write-Host "::set-output name=versionstring::$version"
